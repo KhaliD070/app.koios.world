d004e6a8a4ad1bc1a29576b1b1504add737f0f1d
Corwin <corwinvandalen@gmail.com>
2021-09-02 12:58:49 +0200

    Removed comments

--- src/components/Web3/ConnectionCheck.tsx
+++ src/components/Web3/ConnectionCheck.tsx
@@ -4,18 +4,6 @@ import {
   provider,
 } from "./Web3";
 
-// export const ConnectionCheck = async () => {
-//   if (provider) {
-//     await disconnectWeb3();
-//     console.log("disconnectect");
-//     return provider;
-//   } else {
-//     await connectWeb3();
-//     console.log("connected");
-//     return provider;
-//   }
-// };
-
 export const Connect = async () => {
   await connectWeb3();
   return provider;
