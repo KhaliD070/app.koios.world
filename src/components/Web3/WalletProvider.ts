a8aa16e6784574e4ec4f5aa650e1e350c3358641
Corwin <corwinvandalen@gmail.com>
2021-09-02 12:54:10 +0200

    Only use metamask as legitimate provider

--- src/components/Web3/WalletProvider.ts
+++ src/components/Web3/WalletProvider.ts
@@ -1,12 +1,13 @@
-import WalletConnectProvider from '@walletconnect/web3-provider'
 import Web3Modal from 'web3modal'
 
 const providerOptions = {
-  walletconnect: {
-    package: WalletConnectProvider,
-    options: {
-      infuraId: 'e87f83fb85bf4aa09bdf6605ebe144b7'
-    }
+  injected: {
+    display: {
+      logo: "data:image/gif;base64,INSERT_BASE64_STRING",
+      name: "Injected",
+      description: "Connect with the provider in your Browser"
+    },
+    package: null
   }
 }
 
--- src/components/Web3/Web3.ts
+++ src/components/Web3/Web3.ts
@@ -2,6 +2,7 @@ import Web3 from "web3";
 import { web3Modal } from './WalletProvider';
 import { FindProfile } from './FindProfile';
 import { fetchImage } from "./Ipfs";
+import { getInjectedProvider } from "web3modal";
 
 export let provider;
 export let selectedAccount;
@@ -25,9 +26,14 @@ const fetchAccountData = async () => {
 
 export const connectWeb3 = async () => {
   try {
-    provider = await web3Modal.connect();
-    if(!selectedAccount)
-      await fetchAccountData();
+    if (getInjectedProvider()) {
+      provider = await web3Modal.connect();
+      if (!selectedAccount)
+        await fetchAccountData();
+    }
+    else {
+      console.log("No metamask detected!");
+    }
   } catch (e) {
     console.log("Could not get a wallet connection", e);
   }
@@ -37,9 +43,9 @@ const firstRun = async () => {
   if (web3Modal.cachedProvider) {
     await connectWeb3();
   }
-  }
+}
 
-  firstRun();
+firstRun();
 
 export const disconnectWeb3 = async () => {
   if (provider) {
