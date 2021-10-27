77b13973fe2070898b6904f9df24b2e458639fca
Khalid Hajjari <khalid.hajjari98@outlook.com>
2021-10-27 14:18:14 +0200

    commit

---
+++ src/components/Field.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { IFormContext, FormContext } from "./Form";
+
+
+/* The available editors for the field */
+type Editor = "textbox" | "multilinetextbox" | "dropdown";
+
+export interface IFieldProps {
+  /* The unique field name */
+  id: string;
+
+  /* The label text for the field */
+  label?: string;
+
+  /* The editor for the field */
+  editor?: Editor;
+
+  /* The drop down items for the field */
+  options?: string[];
+
+  /* The field value */
+  value?: any;
+}
+
+export const Field: React.SFC<IFieldProps> = ({
+  id,
+  label,
+  editor,
+  options,
+  value
+}) => {
+return (
+    <FormContext.Consumer>
+      {(context: IFormContext) => (
+        <div className="form-group">
+          
+
+
+
+          {editor! === "multilinetextbox" && (
+            <textarea
+              id={id}
+              value={value}
+              onChange={
+                (e: React.FormEvent<HTMLTextAreaElement>) =>
+                  context.setValues({ [id]: e.currentTarget.value })
+              }
+              onBlur={
+                (e: React.FormEvent<HTMLTextAreaElement>) =>
+                  console.log(e) /* TODO: validate field value */
+              }
+              className="form-control"
+            />
+          )}
+
+
+
+          
+        </div>
+      )}
+    </FormContext.Consumer>
+);
+                }
+
+
+// Field.defaultProps = {
+//   editor: "textbox"
+// };
+
+// export default Field;
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -6,7 +6,16 @@ export const Header = () => {
     <header className="header container">
       <Link to="/" className="header__img-container">
         <img className="header__img" src={koiosLogo} alt="Koios logo" />
+        
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossorigin="anonymous"
+ 
       </Link>
+
+
+ 
     </header>
   );
 };
